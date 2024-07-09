706f5e7b49998701e71e22ea29b9a14d7d1c8f4d
rutiKind <R583209640@gmail.com>
2024-07-09 08:41:42 +0300

    created gh page

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -12,21 +12,21 @@ const App = () => {
   //const isAuthenticated = sessionStorage.getItem('userId')
   const navigate = useNavigate();
 
-  useEffect(() => {
+  // useEffect(() => {
 
     
-    if (!sessionStorage.getItem("userId")) {
-      navigate('/Login');
-    }
-  }, [navigate]);
+  //   if (!sessionStorage.getItem("userId")) {
+  //     navigate('/Login');
+  //   }
+  // }, [navigate]);
 
  
 
   return (
     <>
-     <Nav></Nav>
+     {/* <Nav></Nav> */}
       {/* <Outlet /> */}
-    
+    <p>hello</p>
       <Outlet />
     </>
   );
