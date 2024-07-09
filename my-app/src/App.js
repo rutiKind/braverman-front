d0c17fac96912f813ad5a15040f03744f85dfe78
rutiKind <R583209640@gmail.com>
2024-07-09 08:51:05 +0300

    created gh page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,9 +8,9 @@ import { Outlet, useNavigate } from 'react-router-dom';
 import Nav from './Component/nav/nav.component';
 
 const App = () => {
-  const isAuthenticated = useSelector((state: { user: { isAuthenticated: boolean } }) => state.user.isAuthenticated);
+  // const isAuthenticated = useSelector((state: { user: { isAuthenticated: boolean } }) => state.user.isAuthenticated);
   //const isAuthenticated = sessionStorage.getItem('userId')
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
   // useEffect(() => {
 
